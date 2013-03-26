01be22974dc5d701a7a038f8b8bca5c047fae615
Freddie Juza <xabbuj@gmail.com>
2013-03-26 23:30:52 +0100

    Added some debug output to requesthandler.php

--- requesthandler.php
+++ requesthandler.php
@@ -1,19 +1,31 @@
 <?php
-
+$DEBUG = "true";
 include_once('functions/mysqli_conn.php');
 include_once('functions/func.php');
 include('functions/variables.php');
 if(isset($_POST['requests']) && isset($_POST['Requester']) && isset($_POST['Technician']) && isset($_POST['technician_key']) ){
-	
+	if($DEBUG == "true"){
+		echo "test";
+	}
 	$requests = $_POST['requests'];
 	$requester = $_POST['Requester'];
 	$technician = $_POST['Technician'];
 	$techniciankey = $_POST['technician_key'];
 	$close_request = $_POST['close_request'];
-		
+	if($DEBUG == "true"){
+		echo "Requests: " . $requests . "<br />";
+		echo "Requester: " . $requester . "<br />";
+		echo "Technician: " . $technician . "<br />";
+		echo "Technician Key" . $techniciankey . "<br />";
+		echo "Close Request?" . $close_request . "<br />";
+	}
 	$SQLquery = "SELECT * FROM incidents WHERE reqTemplate = " . $requests;
+	if($DEBUG == "true"){
+		echo "SQL query: " . $SQLquery;
+	}
 		$data = get_mysqliData($SQLquery);
 		while($r = $data->fetch_assoc()){
+			
 		}
 		if(isset($data)){
 			foreach($data as $x=>$x_val){
