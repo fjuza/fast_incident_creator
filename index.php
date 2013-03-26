691bb89cd219c053726f43ae705375b7d414c27a
Freddie Juza <xabbuj@gmail.com>
2013-03-26 22:49:43 +0100

    Added style.css and some chances to index.php

--- index.php
+++ index.php
@@ -10,32 +10,49 @@
 		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
-		<form action="requesthandler.php" method="post">
-			<span id="text_form">Request Type:</span>
-			<select id="Requests">
-				<?php
-				$sql = "SELECT requesttemplate FROM incidents";
-				$output = get_mysqliData($sql);
+		<div id="stylized" class="commitForm">
+			<form action="requesthandler.php" method="post" id="form" name="form">
+			
+				<label>Request Type
+				<span id="text_form">What type of request has been submitted?</span>
+				</label>
+				<select id="Requests">
+					<?php
+					$sql = "SELECT requesttemplate FROM incidents";
+					$output = get_mysqliData($sql);
+					
+						while($row = $output->fetch_assoc()){
+							echo "<option value='" . $row['requesttemplate'] . "'>".$row['requesttemplate'] . "</option>";
+						}
+					?>
+				</select>
 				
-					while($row = $output->fetch_assoc()){
-						echo "<option value='" . $row['requesttemplate'] . "' />";
-					}
-
-				?>
-			</select>
-			<span class="text_form">Technician Key:</span>
-			<select id="technician_key">
-				<option value="3F394415-32F0-4FD9-F44D-977A-C56F44D5A6F1" />
-				<option value="79C4E7D5-4BD9-427D-5050-BE56-4335050702AC" />
-			</select>
-			<span class="text_form">Requester: </span>
-			<input type="text" name="Requester" />
-			<span class="text_form">Technician: </span>
-			<input type="text" name="Technician" />
-			<span class="text_form">Close Request: </span>
-			<input type="checkbox" value="true" name="close_request" />
-			<input type="submit" value="Add Request" name="add_request" />
-		</form>
+				<label>Technician Key
+				<span class="text_form">Select the technician key.</span>
+				</label>
+				<select id="technician_key">
+					<option value="3F394415-32F0-4FD9-F44D-977A-C56F44D5A6F1">Freddie</option>
+					<option value="79C4E7D5-4BD9-427D-5050-BE56-4335050702AC">Erik</option>
+				</select>
+				
+				<label>Requester
+				<span class="text_form">Who submitted the request.</span>
+				</label>
+				
+				<input type="text" name="Requester" />
+				<label>Technician
+				<span class="text_form">Who recievied the request.</span>
+				</label>
+				<input type="text" name="Technician" />
+				
+				<label>Close Request
+				<span class="text_form">Should the request be closed aswell?</span>
+				</label>
+				
+				<input type="checkbox" value="true" name="close_request" />
+				<input type="submit" value="Add Request" name="add_request" />
+			</form>
+		</div>
 		<?php
 
 		?>
